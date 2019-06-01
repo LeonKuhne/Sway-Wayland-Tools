d1306874ba40c58955d4e705b2967b7fa4975266
Leon Kuhne <lxk1170@rit.edu>
2019-06-01 08:14:00 -0400

    added multiple binding types

---
+++ run.py
@@ -0,0 +1,132 @@
+import rtmidi
+import argparse
+import os
+import subprocess
+
+#
+# Helper Functions
+#
+
+def setWorkspace(workspaceName):
+    if(workspaceName != None):
+        os.system(f'sway workspace {workspaceName}')
+
+def execCommand(command):
+    return subprocess.Popen(command, shell=True)
+
+# 
+# BINDING TYPES
+#
+
+class SingleBind:
+    def __init__(self, command, workspace=None, newWorkspace=None):
+        self.command = command
+        self.workspace = workspace
+        self.newWorkspace = newWorkspace
+    
+    def open(self):
+        setWorkspace(self.workspace)
+        setWorkspace(self.newWorkspace)
+        return execCommand(self.command)
+    def close(self):
+        pass
+
+class ToggleBind(SingleBind):
+    def __init__(self, command, workspace=None, newWorkspace=None):
+        SingleBind.__init__(self, command, workspace, newWorkspace)
+        self.process = None
+    def open(self):
+        self.process = SingleBind.open()
+    def close(self):
+        if self.process != None:
+            print("closing bind")
+            self.process.terminate()
+            self.process = None
+        else:
+            print("already closed")
+
+class ReleaseBind(SingleBind):
+    def __init__(self, command, releaseCommand, workspace=None, newWorkspace=None):
+        SingleBind.__init(self, command, workspace, newWorkspace)
+        self.releaseCommand = releaseCommand
+    def close(self):
+        execCommand(self.releaseCommand)
+
+#
+# CONFIG
+#
+
+bindings = {
+    'A2': ToggleBind('spotify', 1, "music"),
+    'G2': ToggleBind('firefox', 2, "internet"),
+    'C2': ToggleBind('urxvt -bg black -fg white', 3, "code"),
+    'D2': ToggleBind('urxvt -bg black -fg white'),
+    'E2': ToggleBind('urxvt -bg black -fg white'),
+    'F2': ToggleBind('urxvt -bg black -fg white'),
+    'D3': SingleBind('', 'code'),
+    'B3': SingleBind('', 'music'),
+    'A3': SingleBind('', 'internet')
+}
+port = 2 # 0..n
+readTimeout = 250 # ms
+
+#
+# CODE
+#
+
+# vars
+midiin = rtmidi.RtMidiIn()
+
+# noteExp > the expected note
+def eventController(midi):
+    midi.getNoteNumber()
+    note = midi.getMidiNoteName(midi.getNoteNumber())
+    velocity = midi.getVelocity()
+    
+    # controller logic
+    if note in bindings:
+        binding = bindings[note]
+
+        if midi.isNoteOn():
+            print("opening binding")
+            binding.open()
+        
+        elif midi.isNoteOff():
+            print("closing binding")
+            binding.close()
+
+        elif midi.isController():
+            #print('CONTROLLER', midi.getControllerNumber(), midi.getControllerValue())
+            print("controllers currently unsupported")
+            pass
+    else:
+        print(f"{note} not set")
+
+def listenToPort(port):
+    # connect to midi
+    print(f"Opening port {port}!")
+    midiin.openPort(port)
+
+    # read midi messages
+    while True:
+        event = midiin.getMessage(250) # some timeout in ms
+        if event:
+            eventController(event)
+
+
+# connect 
+if __name__ == '__main__':
+    # parse args
+    parser = argparse.ArgumentParser(description='program midi keyboard bindings')
+    parser.add_argument("-p", "--ports", help="show all connected ports", action="store_true")
+    args = parser.parse_args()
+
+    ports = range(midiin.getPortCount())
+
+    # handle args
+    if args.ports:
+        print(f"Here are the connected midi devices ({ports.len()}):")
+        for i in ports:
+            print(midiin.getPortName(i))
+    else:
+        listenToPort(port)
