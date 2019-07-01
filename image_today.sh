b7fee27139410d4dfd2aee1e25f64bc26365554e
Leon Kuhne <lxk1170@rit.edu>
2019-07-01 19:24:59 -0400

    saving

--- image_today.sh
+++ image_today.sh
@@ -1,11 +1,13 @@
+#!/bin/sh
+
 QUERY=$(date +"%A, %B %d")
 
 # consider using '-R day' flag to get images from today :D
-~/.tools/googliser.sh -p $QUERY -L -a wide -m 4mp -n 1 -o /data/Downloads #nothing should go to downloads, -o uses local dir
-URL=$(head -n 1 ./download.links.list)
+/home/x/.tools/googliser.sh -p "$QUERY $@" -L -a wide -m 4mp -n 1 -o /data/Downloads #nothing should go to downloads, -o uses local dir
+URL=$(head -n 1 /home/x/.tools/download.links.list)
 echo $URL
 wget -O /data/Downloads/image.today $URL
-rm ./download.links.list
+rm /home/x/.tools/download.links.list
 
 # set the backgrounds
 sway output DVI-D-1 bg /data/Downloads/image.today fill
