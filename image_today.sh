2dc2cd886c401f1906eb117cf5d43c10011ebd35
Leon Kuhne <lxk1170@rit.edu>
2020-11-16 10:43:35 -0500

    updated googlizer

--- image_today.sh
+++ image_today.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-#source /home/x/.bash_profile
+# need this for the cronjob
+TOOLS=/home/id/Projects/tools
+IMG_DIR=/home/id
+#source /home/id/.bash_profile
 QUERY=$(date +"%A, %B %d")
 NOUN=$(curl https://www.random-ize.com/noun/nou-f.php)
 # consider using '-R day' flag to get images from today :D
-/home/id/Projects/Sway-Wayland-Tools/googliser.sh -p "$QUERY $NOUN" -L -a wide -m 4mp -n 1 -o /home/id/Downloads #nothing should go to downloads, -o uses local dir
-#URL=$(head -n 1 download.links.list)
-#wget -O /home/id/Downloads/image.today $URL
+/usr/local/bin/googliser -p "$QUERY $NOUN" -L -a wide -m 4mp -n 1 -o $IMG_DIR # it goes into $HOME
+URL=$(head -n 1 $IMG_DIR/image.links.list)
+wget -O /home/id/Downloads/image.today $URL
 
-#rm download.links.list
+echo "found url: $URL"
 
-# get the current active socket
-#export SWAYSOCK=/run/user/$(id -u)/sway-ipc.$(id -u).$(pgrep -x sway).sock
+rm $IMG_DIR/image.links.list
+
+# get the current active socket, for cronjob
+export SWAYSOCK=/run/user/$(id -u)/sway-ipc.$(id -u).$(pgrep -x sway).sock
 
 # show the background image
-#sway output DP-1 bg /home/id/Downloads/image.today fill
-#sway output DVI-D-1 bg /home/id/Downloads/image.today fill
-#sway output DVI-D-2 bg /home/id/Downloads/image.today fill
+sway output DP-1 bg /home/id/Downloads/image.today fill
+sway output DVI-D-1 bg /home/id/Downloads/image.today fill
+sway output DVI-D-2 bg /home/id/Downloads/image.today fill
 #sway output HDMI-A-1 bg /home/id/Downloads/image.today fill
 
 # Split the background up into 4 pieces
+#
+
 # I stole these values from ~/.config/sway/config
 
 # total screen image. The '^' stands for 'minimum values of width and height given, aspect ratio preserved'
@@ -28,11 +35,11 @@ NOUN=$(curl https://www.random-ize.com/noun/nou-f.php)
 #convert -crop 1440x900+1600+0 /home/id/Downloads/image_today.png /home/id/Downloads/image_today_1.png
 #convert -crop 1920x1080+0+960 /home/id/Downloads/image_today.png /home/id/Downloads/image_today_2.png
 #convert -crop 900x1440+1920+900 /home/id/Downloads/image_today.png /home/id/Downloads/image_today_3.png
-convert -crop 1920x1080+2820+900 /home/id/Downloads/image_today.png /home/id/Downloads/image_today_4.png
+#convert -crop 1920x1080+2820+900 /home/id/Downloads/image_today.png /home/id/Downloads/image_today_4.png
 
-# show the background images
-sway output HDMI-A-1 bg /home/id/Downloads/image_today_1.png fill
-sway output DVI-D-2 bg /home/id/Downloads/image_today_2.png fill
-sway output DVI-D-1 bg /home/id/Downloads/image_today_3.png fill
-sway output DP-1 bg /home/id/Downloads/image_today_4.png fill
+# show the split background images
+#sway output HDMI-A-1 bg /home/id/Downloads/image_today_1.png fill
+#sway output DVI-D-2 bg /home/id/Downloads/image_today_2.png fill
+#sway output DVI-D-1 bg /home/id/Downloads/image_today_3.png fill
+#sway output DP-1 bg /home/id/Downloads/image_today_4.png fill
 
